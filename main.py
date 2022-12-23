ba42e15150823ab224119ce80626a93d91e83ee2
Dhruv Vohra <54986592+Dhruvohra@users.noreply.github.com>
2022-12-23 20:27:18 +0530

    Added Comments

--- main.py
+++ main.py
@@ -17,22 +17,18 @@ sender_password = os.environ['Sender_pass']
 recipient_address = os.environ['Recipient']
 email_content = str(os.environ['email_message'])
 
-
-print("content" ,email_content)
-print("type", type(email_content))
-
-print(os.environ['email_message'])
-print(type(os.environ['email_message']))
-
-
 server = smtplib.SMTP(server_type, port)
+print('Establishing server connection')
 #Hardcoding Server Details
 
 server.starttls()
+print('Started server connection')
 #Starting server using transport layer security.
 
 server.login(sender_address, sender_password)
+print('Logged into the Senders account')
 #Gmail Dummy creds for app.
 
 server.sendmail(sender_address, recipient_address, email_content)
+print('Email Sent')
 # This is taking 3 Parameters as arguments as of now Sender Address, recipient Address, Email Body.
