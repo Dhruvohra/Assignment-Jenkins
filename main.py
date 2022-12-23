f5346bf0dff3195085c107069a07dbf2ede6a7a4
Dhruv Vohra <54986592+Dhruvohra@users.noreply.github.com>
2022-12-23 20:04:04 +0530

    Input from Jenkins Parameter added

--- main.py
+++ main.py
@@ -2,30 +2,31 @@ import smtplib
 import os
 #python library to use SMPT protocol for Emails
 
-print(type(os.environ['Server']))
-print(type(os.environ['Sender']))
-print(type(os.environ['Sender_pass']))
-print(type(os.environ['Recipient']))
-print(type(os.environ['email_message']))
+server_type = os.environ['Server']
+
+if server_type == 'smtp.gmail.com':
+  port = 587
+elif server_type == 'smtp.office365.com':
+  port = 587
+elif server_type == 'smtp.mail.yahoo.com':
+  port = 465
+  
+
+sender_address = os.environ['Sender']
+sender_password = os.environ['Sender_pass']
+recipient_address = os.environ['Recipient']
+email_content = os.environ['email_message']
 
 
-print(os.environ['Server'])
-print(os.environ['Sender'])
-print(os.environ['Sender_pass'])
-print(os.environ['Recipient'])
-print(os.environ['email_message'])
 
-"""
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = smtplib.SMTP(server_type, port)
 #Hardcoding Server Details
 
 server.starttls()
 #Starting server using transport layer security.
 
-server.login('awsmanglu@gmail.com', 'uiceviextwufbfbv')
+server.login(sender_address, sender_password)
 #Gmail Dummy creds for app.
 
-server.sendmail('awsmanglu@gmail.com', 'dhruvohra@gmail.com', 'Mail sent from python script run via jenkins using gmail smtp server')
+server.sendmail(sender_address, recipient_address, email_content)
 # This is taking 3 Parameters as arguments as of now Sender Address, recipient Address, Email Body.
-"""
-
