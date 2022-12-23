840a36bfd1091ffa0cf435bf3d1178e974bd87ef
Dhruv Vohra <54986592+Dhruvohra@users.noreply.github.com>
2022-12-23 18:15:20 +0530

    Dummy Email setup with hardcoded values

--- email.py
+++ email.py
@@ -1,2 +1,14 @@
+import smtplib
+#python library to use SMPT protocol for Emails
 
-print("Testing script")
+server = smtplib.SMTP('smtp.gmail.com', 587)
+#Hardcoding Server Details
+
+server.starttls()
+#Starting server using transport layer security.
+
+server.login('awsmanglu@gmail.com', 'uiceviextwufbfbv')
+#Gmail Dummy creds for app.
+
+server.sendmail('awsmanglu@gmail.com', 'dhruvohra@gmail.com', 'Mail sent from python script run via jenkins using gmail smtp server')
+# This is taking 3 Parameters as arguments as of now Sender Address, recipient Address, Email Body.
